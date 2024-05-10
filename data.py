efa165f68f29770be225439b71c38998a65bdfde
hermanjustino <hermanjustino@gmail.com>
2024-05-10 14:25:00 -0400

    Print Eglinton to terminal

--- data.py
+++ data.py
@@ -1,4 +1,9 @@
-from pandas import *
- 
-# reading CSV file
-data = read_csv("/data/data.xlsx")
+import pandas as pd
+
+# Read the Excel file
+df = pd.read_excel("/Users/hermanjustino/Desktop/projects/youth_Summit/city_data/data/data.xlsx")
+
+
+# Print rows 107 to 122
+pd.set_option('display.max_rows', 500)
+print(df.iloc[106:122, [0, df.columns.get_loc("West Humber-Clairville")]])
